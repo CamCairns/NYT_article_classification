fccc576e8592a47bb5c0218e54d428eb581ae373
CamCairns <cam.cairns1@gmail.com>
2015-06-08 20:24:41 +1000

    testing a few different webpage urls

--- nyt_souper.py
+++ nyt_souper.py
@@ -6,9 +6,10 @@ import requests
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-r  = requests.get('http://query.nytimes.com/gst/fullpage.html?res=940DE5D6123DF934A25756C0A9649D8B63')
+r  = requests.get('http://query.nytimes.com/gst/fullpage.html?res=9B07E3DB1438F932A25756C0A9659D8B63')
 data = r.text
 soup = BeautifulSoup(data)
-#print soup.prettify()
-item = soup.find_all([("p", {"class":"story-body-text story-content"}), ("p", {"itemprop":"articleBody"})])
+# print soup.prettify()
+item = soup.find_all("p", {"class":["story-body-text story-content","story-body-text"]})
+item.extend(soup.find_all(["p", {"itemprop":"articleBody"}]))
 print item
