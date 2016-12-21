c2d44b3b9bd536dc117c55253b2341f713b75527
CamCairns <cam.cairns1@gmail.com>
2016-12-21 22:27:01 +1100

    Updated create_corpus with relative path

--- generate_corpus/bin/create_corpus.sh
+++ generate_corpus/bin/create_corpus.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # extract corpus from nyt api
-python nyt_corpus_creator.py
+python ../nyt_corpus_creator.py
 
 # clean up extraneous lines
-python nyt_article_remove_line.py
+python ../nyt_article_remove_line.py
